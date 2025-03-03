ba7d12d6453f7f43398fc1aca879f2d303afc51f
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-03-03 16:31:15 +0200

    AboutUs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.scss';
+import AboutUs from './assets/components/AboutUs';
 import Header from './assets/components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <AboutUs />
+      </main>
     </>
   );
 };
