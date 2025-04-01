af2299bc1ebec47eb77f85fa7c260789468e590a
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-01 12:01:27 +0300

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import debounce from 'lodash.debounce';
+import cn from 'classnames';
 import Switch from './assets/components/Switch';
 
 const API_KEY = '71c9703614e3f98df03507b272cbfa49';
 
 const lightThemeClasses = ['from-sky-300', 'to-blue-500'];
-const darkThemeClasses = ['from-gray-800', 'to-blue-950'];
+const darkThemeClasses = ['from-gray-800', 'to-blue-900'];
+
+const lightThemeCardClasses = ['from-sky-400', 'to-blue-600'];
+const darkThemeCardClasses = ['from-gray-900', 'to-blue-950'];
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState({});
@@ -88,8 +92,20 @@ function App() {
           onChange={handleChangeInputValue}
         />
 
-        <article className="bg-gradient-to-r from-sky-400 to-blue-600 p-4 w-full sm:max-w-2xl rounded-2xl shadow-lg from-gray-800 text-white">
-          Lorem ipsum dolor sit .
+        <article
+          className={cn(
+            'flex flex-col sm:flex-row gap-3 bg-gradient-to-r  p-4 w-full sm:max-w-2xl rounded-2xl shadow-lg text-white transition duration-500 easy-in delay-100',
+            theme === 'light' ? lightThemeCardClasses : darkThemeCardClasses
+          )}
+        >
+          <div>
+            <span className="text-7xl">27°</span>
+          </div>
+
+          <div className="flex flex-col">
+            <span className="text-sm">Tuesday, 23 December</span>
+            <span className="text-sm">Kiev, Ukraine</span>
+          </div>
         </article>
       </main>
     </>
